8b49e77bb59841fb709cc15e0baac197e9d6d836
Drolejan <drolejan@gmail.com>
2026-03-12 09:55:34 -0600

    prueba

--- Assets/Codigos/movimientoNuevo.cs
+++ Assets/Codigos/movimientoNuevo.cs
@@ -1,31 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class movimientoViejo : MonoBehaviour
+public class movimientoNuevo : MonoBehaviour
 {
     Rigidbody2D playerRB;//Variable para el rigidbody de nuestro player
     public float velPlayer;
     public float velSalto;
     public GameObject bala; //El objeto que vamos a disparar
     public float balaSpeed;
-    // Asi funciona el viejo input, pero despues de esto usaremos
+    PlayerInput controles;
     void Start()
     {
        playerRB=GetComponent<Rigidbody2D>(); 
+       controles=GetComponent<PlayerInput>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float movimientoH=Input.GetAxis("Horizontal");
+        float movimientoH=controles.actions["Move"].ReadValue<Vector2>().x;
         playerRB.linearVelocityX=movimientoH*velPlayer;
 
-        if (Input.GetButtonDown("Jump"))
+        if (controles.actions["Jump"].WasPressedThisFrame())
         {
             playerRB.AddForceY(velSalto,ForceMode2D.Impulse);
         }
 
-        if (Input.GetButtonDown("Fire1"))
+        if (controles.actions["Attack"].WasPressedThisFrame())
         {
             GameObject estaBala=Instantiate(bala,transform.position,Quaternion.identity);
             Rigidbody2D balaRB=estaBala.GetComponent<Rigidbody2D>();

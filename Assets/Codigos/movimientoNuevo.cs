3c821a87147418b81bd0f208e6f6564550f1de1a
Drolejan <drolejan@gmail.com>
2026-03-19 09:41:02 -0600

    disparoCorregido

--- Assets/Codigos/movimientoNuevo.cs
+++ Assets/Codigos/movimientoNuevo.cs
@@ -9,6 +9,8 @@ public class movimientoNuevo : MonoBehaviour
     public GameObject bala; //El objeto que vamos a disparar
     public float balaSpeed;
     PlayerInput controles;
+    int direction=1;//Volteando a la izquierda
+    public Transform origenDisparo;
     void Start()
     {
        playerRB=GetComponent<Rigidbody2D>(); 
@@ -21,6 +23,17 @@ public class movimientoNuevo : MonoBehaviour
         float movimientoH=controles.actions["Move"].ReadValue<Vector2>().x;
         playerRB.linearVelocityX=movimientoH*velPlayer;
 
+        if (movimientoH > 0)
+        {
+            direction=-1;
+            transform.localScale=new Vector2(direction,transform.localScale.y);
+        }
+        else if (movimientoH < 0)
+        {
+            direction=1;
+            transform.localScale=new Vector2(direction,transform.localScale.y);
+        }
+
         if (controles.actions["Jump"].WasPressedThisFrame())
         {
             playerRB.AddForceY(velSalto,ForceMode2D.Impulse);
@@ -28,9 +41,10 @@ public class movimientoNuevo : MonoBehaviour
 
         if (controles.actions["Attack"].WasPressedThisFrame())
         {
-            GameObject estaBala=Instantiate(bala,transform.position,Quaternion.identity);
+            GameObject estaBala=Instantiate(bala,origenDisparo.position,Quaternion.identity);
             Rigidbody2D balaRB=estaBala.GetComponent<Rigidbody2D>();
-            balaRB.AddForceX(balaSpeed,ForceMode2D.Impulse);
+            balaRB.AddForceX(balaSpeed*-direction,ForceMode2D.Impulse);
+            Destroy(estaBala,2f);//Despues de 2 seg se destruye la bala 
         }
     }
 }
